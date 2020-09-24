39019ee471457932ce30ee694cb0cb90b14be2ab
Miguel Ángel González Santamarta <mgons@unileon.es>
2020-09-24 11:14:41 +0200

    scripts to install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y python-dev libportaudio2 libportaudiocpp0 portaudio19-dev libasound-dev swig
+sudo pip3 install pyaudio SpeechRecognition google-cloud-speech
+sudo pip3 install setuptools wheel pocketsphinx
+sudo pip3 install install nltk
+sudo pip3 install contractions TextSearch
+sudo pip3 install pyparsing pyjsgf
+python3 ./nltk_download.py
